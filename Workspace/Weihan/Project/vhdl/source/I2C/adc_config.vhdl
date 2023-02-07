c20a00b36acf79b0d7272b660341c857aaddea89
Gao Weihan <gwh2022ok@yeah.net>
2023-02-07 20:33:18 +0100

    adc_config v1.8

--- Workspace/Weihan/Project/vhdl/source/I2C/adc_config.vhdl
+++ Workspace/Weihan/Project/vhdl/source/I2C/adc_config.vhdl
@@ -118,32 +118,32 @@ begin  -- architecture arch_adc_config
   -- purpose: waiting 
   waitingtime_proc: process (clk, rstn) is
     variable k : integer := 0;        -- Parameter passing
-   begin
-     k := waiting_time;
-     if rstn = '0' then                 -- asynchronous reset (active low)
-       cnt_clk <= 0;
-       flag_cnt_STOP <= '0';
-     elsif clk'event and clk = '1' then  -- rising clock edge
-       if SHDNZ_pin='1' then
-         if flag_cnt_START='1' then
-           if cnt_clk < k then
-             cnt_clk <= cnt_clk+1;
-             flag_cnt_STOP <= '0';
-           else
-             cnt_clk <= 0;
-             flag_cnt_STOP <= '1';
-           end if;
-         else
-           flag_cnt_STOP <= '0';
-           cnt_clk <= 0;
-         end if;
-       else
-         cnt_clk <= 0;
-         flag_cnt_STOP <= '0';
-       end if;
-     end if;
-   end process waitingtime_proc;
-   
+  begin
+    k := waiting_time;
+    if rstn = '0' then                 -- asynchronous reset (active low)
+      cnt_clk <= 0;
+      flag_cnt_STOP <= '0';
+    elsif clk'event and clk = '1' then  -- rising clock edge
+      if SHDNZ_pin='1' then
+        if flag_cnt_START='1' then
+          if cnt_clk < k then
+            cnt_clk <= cnt_clk+1;
+            flag_cnt_STOP <= '0';
+          else
+            cnt_clk <= 0;
+            flag_cnt_STOP <= '1';
+          end if;
+        else
+          flag_cnt_STOP <= '0';
+          cnt_clk <= 0;
+        end if;
+      else
+        cnt_clk <= 0;
+        flag_cnt_STOP <= '0';
+      end if;
+    end if;
+  end process waitingtime_proc;
+  
   ----------------------------------------
   -- purpose: fsm
   state_transition_proc: process (clk, rstn) is
@@ -164,83 +164,114 @@ begin  -- architecture arch_adc_config
         else
           next_state <= idle_state;
         end if;
-      when  hardware_shutdown_state =>
+      when  hardware_shutdown_state =>  --2b start
         if SHDNZ_pin = '1' then
           next_state <=  SHDNZ_state;
         else
           next_state <=  hardware_shutdown_state;
         end if;
-      when SHDNZ_state =>
+      when SHDNZ_state =>               --2b stop
         if flag_cnt_STOP = '1' then
           next_state <=  wakeup_state;
         else
           next_state <=  SHDNZ_state;
         end if;
-      when wakeup_state  =>
+      when wakeup_state  =>             --3a and 
         if done = '1' then
-          next_state <= powerdown_state ;
+          next_state <= woke_state ;
         else
           next_state <= wakeup_state;
         end if;
-
-
-        
-      -- 2/7
-      when powerdown_state  =>
+      when woke_state =>                --3b stop
+        if flag_cnt_STOP = '1' then
+          next_state <=  powerdown_state;
+        else
+          next_state <=  woke_state;
+        end if;
+      when powerdown_state  =>          --3c
         if done = '1' then
           next_state <= config_channel_state;
         else
           next_state <= powerdown_state;
         end if;
-      when  config_channel_state =>
-        if cnt_channel <= CHANNEL_NUMBER then
+      when  config_channel_state =>     --3c
+        -- if cnt_channel <= CHANNEL_NUMBER then
+        --   next_state <= config_channel_state;
+        -- else
+        --   next_state <= enable_input_state;
+        -- end if;
+        if done = '1' then              --only for 1 channel
+          next_state <= enable_input_state;
+        else
           next_state <= config_channel_state;
+        end if;
+      when  enable_input_state =>       --3d
+        if done = '1' then
+          next_state <= enable_output_state;
         else
           next_state <= enable_input_state;
-        end if;  
-      when  enable_input_state => 
+        end if;
+      when  enable_output_state =>      --3e
+        if done = '1' then
+          next_state <= powerup_state;
+        else
           next_state <= enable_output_state;
-      when  enable_output_state => 
-          next_state <= powerup_state;    
-      when  powerup_state=> 
-        next_state <=  APPLY_BCLK_FSYNC_state;
-      when  APPLY_BCLK_FSYNC_state => 
-        next_state <=  enable_diagnostics_state;
-      when  enable_diagnostics_state =>  
-        next_state <=  disable_diagnostics_state;
+        end if;
+      when  powerup_state=>             --3f
+        if done = '1' then
+          next_state <= APPLY_BCLK_FSYNC_state;
+        else
+          next_state <= powerup_state;
+        end if;
+      --------------------------------------------------------------------------------
+      when  APPLY_BCLK_FSYNC_state =>   --3g and 3h and 3i start
+        next_state <=  I2S_working_state;
+        --
+        --
+      --------------------------------------------------------------------------------
+      when I2S_working_state =>         --3i stop
+        if flag_cnt_STOP = '1' then
+          next_state <=  enable_diagnostics_state;
+        else
+          next_state <=  I2S_working_state;
+        end if;
+      when  enable_diagnostics_state => --3j
+        if done = '1' then
+          -- next_state <= disable_diagnostics_state;
+          next_state <= stop_state;
+        else
+          next_state <= enable_diagnostics_state;
+        end if;
       when  disable_diagnostics_state =>
-        next_state <=  stop_state;
+        --next_state <=  stop_state;
+        --
       when stop_state =>
         next_state <=  end_state;
+      when end_state =>
       when others => null;
     end case;
   end process;
 
   assignment_proc: process (state,cnt_channel, SW_vdd_ok,SHDNZ_pin,done) is
   begin  -- process assignment_proc
+    OUT_config_value <= x"00";
     case state is
       when idle_state =>
-        flag_cnt_START <= '0';
-        waiting_time <= 0;
-        OUT_start <= '0';
-      when hardware_shutdown_state =>
+        flag_cnt_START  <= '0';
+        waiting_time    <= 0;
+        OUT_start       <= '0';
+      when hardware_shutdown_state =>   --2b start
         if SHDNZ_pin = '1' then
-          flag_cnt_START <= '1';
+          flag_cnt_START <= '1';        
           waiting_time <= WTIME_1ms_CNT;
         else
           flag_cnt_START <= '0';
           waiting_time <= 0;
         end if;
         OUT_start <= '0';
-      when SHDNZ_state =>
+      when SHDNZ_state =>               --2b stop
         OUT_start <= '0';
-
-
-
-
-
-      -- 2/7
-      when wakeup_state  =>
+      when wakeup_state  =>             --3a
         flag_cnt_START <= '0';
         waiting_time <= 0;
         OUT_config_value <= value_wakeup;
@@ -249,8 +280,11 @@ begin  -- architecture arch_adc_config
         else
           OUT_start <= '1';
         end if;
-        
-      when powerdown_state  =>
+      when woke_state =>                --3b
+        flag_cnt_START <= '1';        
+        waiting_time <= WTIME_1ms_CNT;
+        OUT_start <= '0';
+      when powerdown_state  =>          --3c
         flag_cnt_START <= '0';
         waiting_time <= 0;
         OUT_config_value <= value_powerdown;
@@ -259,8 +293,7 @@ begin  -- architecture arch_adc_config
         else
           OUT_start <= '1';
         end if;
-
-      when config_channel_state => 
+      when config_channel_state =>      --3c
         -- if cnt_channel = 1 then
         --   OUT_config_value <= value_c1_config;
         -- elsif cnt_channel = 2 then
@@ -272,24 +305,61 @@ begin  -- architecture arch_adc_config
         -- else
         --   OUT_config_value <= "00000000";
         -- end if;
-        OUT_config_value <= value_c1_config;
-        OUT_start <= '1';
-      when  enable_input_state => 
+        -- OUT_config_value <= value_c1_config;
+        -- OUT_start <= '1';
+        flag_cnt_START <= '0';
+        waiting_time <= 0;
+        OUT_config_value <= value_c1_config;  -- we just do only one channel
+        if done='1' then
+          OUT_start <= '0';
+        else
+          OUT_start <= '1';
+        end if;
+      when  enable_input_state =>       --3d
         OUT_config_value <= value_enable_input;
-        OUT_start <= '1';
-      when enable_output_state =>
+        if done='1' then
+          OUT_start <= '0';
+        else
+          OUT_start <= '1';
+        end if;
+      when enable_output_state =>       --3e
         OUT_config_value <= value_enable_output;
-        OUT_start <= '1';
-      when  powerup_state=> 
+        if done='1' then
+          OUT_start <= '0';
+        else
+          OUT_start <= '1';
+        end if;
+      when  powerup_state=>             --3f
         OUT_config_value <= value_powerup;
-        OUT_start <= '1';
-      when  APPLY_BCLK_FSYNC_state => 
+        if done='1' then
+          OUT_start <= '0';
+        else
+          OUT_start <= '1';
+        end if;
+      --------------------------------------------------------------------------------
+      when  APPLY_BCLK_FSYNC_state =>   --3g and 3h and 3i start
+        flag_cnt_START <= '1';        
+        waiting_time <= WTIME_1ms_CNT;
+        OUT_start <= '0';
+      --------------------------------------------------------------------------------
+      when I2S_working_state =>         --3i stop
         
-      when  enable_diagnostics_state =>  
+        OUT_start <= '0';
+        
+      when  enable_diagnostics_state => --3j
+        flag_cnt_START <= '0';
+        waiting_time <= 0;
         OUT_config_value <=  value_enable_diagno;
-      when  disable_diagnostics_state =>
-        
+        if done='1' then
+          OUT_start <= '0';
+        else
+          OUT_start <= '1';
+        end if;
+      when  disable_diagnostics_state => --3j
+        OUT_start <= '0';
       when stop_state =>
+        OUT_start <= '0';
+      when end_state =>
         OUT_start <= '0';
       when others => null;
     end case;
