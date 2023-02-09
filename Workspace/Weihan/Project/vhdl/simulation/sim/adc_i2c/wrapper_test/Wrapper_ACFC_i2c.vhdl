0eb90cd48fdda1a6a303a199fbe3fe056350406c
Gao Weihan <gwh2022ok@yeah.net>
2023-02-09 23:21:15 +0100

    controller-i2c combiled and finished

--- Workspace/Weihan/Project/vhdl/simulation/sim/adc_i2c/wrapper_test/Wrapper_ACFC_i2c.vhdl
+++ Workspace/Weihan/Project/vhdl/simulation/sim/adc_i2c/wrapper_test/Wrapper_ACFC_i2c.vhdl
@@ -6,7 +6,7 @@
 -- Author     : weihanga@chalmers.se
 -- Company    : 
 -- Created    : 2023-02-08
--- Last update: 2023-02-08
+-- Last update: 2023-02-09
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -27,17 +27,22 @@ entity Wrapper_ACFC_i2c is
   
   port (
     -- clk and rstn
-    clk       : in    std_logic;
-    rstn      : in    std_logic;
+    clk         : in    std_logic;
+    rstn        : in    std_logic;
     -- control ready
-    SW_vdd_ok : in    std_logic;
-    SHDNZ     : in    std_logic;
-    -- i2c communication
-    SDA       : inout std_logic;
-    SCL       : out   std_logic
+    SW_vdd_ok   : in    std_logic;
+    SHDNZ_ready : in    std_logic;
+    
     -- FSNYC and BCLK
-    --FSNYC     : out   std_logic;
-    --BCLK      : out   std_logic
+    FSYNC     : out   std_logic;
+    BCLK      : out   std_logic;
+    
+    -- i2c communication
+    SDA         : inout std_logic;
+    SCL         : out   std_logic;
+    -- PLL
+    SHDNZ       : out   std_logic
+    
     );
 
 end entity Wrapper_ACFC_i2c;
@@ -46,19 +51,19 @@ end entity Wrapper_ACFC_i2c;
 architecture arch_Wrapper_ACFC_i2c of Wrapper_ACFC_i2c is
   -- component
   component adc_config is
-    generic(
-      -- MODE reg addr
-      constant const_addr_sleep_reg         : std_logic_vector(7 downto 0) := x"02";
-      constant const_addr_interrupt_reg     : std_logic_vector(7 downto 0) := x"28";
-      constant const_addr_c1_reg            : std_logic_vector(7 downto 0) := x"3c";
-      constant const_addr_c2_reg            : std_logic_vector(7 downto 0) := x"41";
-      constant const_addr_c3_reg            : std_logic_vector(7 downto 0) := x"46";
-      constant const_addr_c4_reg            : std_logic_vector(7 downto 0) := x"4b";
-      constant const_addr_input_channel_reg : std_logic_vector(7 downto 0) := x"73";
-      constant const_addr_8_reg             : std_logic_vector(7 downto 0) := x"74";
-      constant const_addr_powerup_reg       : std_logic_vector(7 downto 0) := x"75";
-      constant const_addr_10_reg            : std_logic_vector(7 downto 0) := x"64"
-      );
+    -- generic(
+    --   -- MODE reg addr
+    --   constant const_addr_sleep_reg         : std_logic_vector(7 downto 0) := x"02";
+    --   constant const_addr_interrupt_reg     : std_logic_vector(7 downto 0) := x"28";
+    --   constant const_addr_c1_reg            : std_logic_vector(7 downto 0) := x"3c";
+    --   constant const_addr_c2_reg            : std_logic_vector(7 downto 0) := x"41";
+    --   constant const_addr_c3_reg            : std_logic_vector(7 downto 0) := x"46";
+    --   constant const_addr_c4_reg            : std_logic_vector(7 downto 0) := x"4b";
+    --   constant const_addr_input_channel_reg : std_logic_vector(7 downto 0) := x"73";
+    --   constant const_addr_8_reg             : std_logic_vector(7 downto 0) := x"74";
+    --   constant const_addr_powerup_reg       : std_logic_vector(7 downto 0) := x"75";
+    --   constant const_addr_10_reg            : std_logic_vector(7 downto 0) := x"64"
+    --   );
     port (
       rstn         : in  std_logic;
       clk          : in  std_logic;
@@ -66,7 +71,9 @@ architecture arch_Wrapper_ACFC_i2c of Wrapper_ACFC_i2c is
       start        : out std_logic;
       config_value : out std_logic_vector(7 downto 0);
       config_addr  : out std_logic_vector(7 downto 0);
-      SHDNZ        : in  std_logic;
+
+      SHDNZ        : out std_logic;
+      SHDNZ_ready  : in  std_logic;
       SW_vdd_ok    : in  std_logic
       );
   end component adc_config;
@@ -86,6 +93,15 @@ architecture arch_Wrapper_ACFC_i2c of Wrapper_ACFC_i2c is
       );
   end component adc_i2c_controller;
 
+--component design_1_PLL is
+--  port (
+--    BCLK : out STD_LOGIC;
+--    FSYNC : out STD_LOGIC;
+--    reset : in STD_LOGIC;
+--    sys_clock : in STD_LOGIC
+--  );
+--  end component design_1_PLL;
+
   ------------------------------------------------------------
   -- signal between i2c and ACFC
   signal done : std_logic;
@@ -95,11 +111,20 @@ architecture arch_Wrapper_ACFC_i2c of Wrapper_ACFC_i2c is
   -- out signal for FSYNC and BCLK
   signal out_FSYNC : std_logic;
   signal out_BCLK : std_logic;
+
+  signal out_n_clk : std_logic;
+  signal out_p_clk : std_logic;
+
   
+    
 begin  -- architecture arch_Wrapper_ACFC_i2c
   
   --FSYNC <=  out_FSYNC;
   --BCLK <=  out_BCLK;
+
+  out_p_clk <= not(out_n_clk);
+  out_n_clk <= clk;
+  
   ------------------------------------------------------------
   -- INSTANCE
   inst_ACFC: component adc_config
@@ -111,6 +136,7 @@ begin  -- architecture arch_Wrapper_ACFC_i2c
       config_value => config_value,
       config_addr  => config_addr,
       SHDNZ        => SHDNZ,
+      SHDNZ_ready  => SHDNZ_ready,
       SW_vdd_ok    => SW_vdd_ok);
   inst_i2c: component adc_i2c_controller
     port map (
@@ -122,7 +148,16 @@ begin  -- architecture arch_Wrapper_ACFC_i2c
       done  => done,
       SDA   => SDA,
       SCL   => SCL );
+      
   ------------------------------------------------------------
-  --GENERATE the FSYNC and BCLK
-  --
+  ----GENERATE the FSYNC and BCLK by PLL
+--   pll_for_i2s_FSYNC_BCLK: component design_1_PLL
+--   port map (
+--      BCLK  => BCLK,
+--      FSYNC  => FSYNC,
+--      reset  => rstn,
+--      sys_clock   => out_n_clk);
+  ------------------------------------------------------------
+  
+  
 end architecture arch_Wrapper_ACFC_i2c;
