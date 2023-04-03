06d5514ad870d8f484111120e718b52a4f163f98
Daniel3E <124577043+Daniel3E@users.noreply.github.com>
2023-04-03 11:37:35 +0200

    Update algorithm_simple_max.m

--- Workspace/Lexuan/Project/matlab/source/algorithm_simple_max.m
+++ Workspace/Lexuan/Project/matlab/source/algorithm_simple_max.m
@@ -1,19 +1,18 @@
-clc, clear all, close all
+%clc, clear all, close all
 
 %run("soundstage2.m"); 
-run("soundExporter.m");
 iY=mY;
 close all
 
 iY = abs(iY);
-subplot(4,1,1);
-plot(iY(:,1));
-subplot(4,1,2);
-plot(iY(:,2));
-subplot(4,1,3);
-plot(iY(:,3));
-subplot(4,1,4);
-plot(iY(:,4));
+%subplot(4,1,1);
+%plot(iY(:,1));
+%subplot(4,1,2);
+%plot(iY(:,2));
+%subplot(4,1,3);
+%plot(iY(:,3));
+%subplot(4,1,4);
+%plot(iY(:,4));
 
 sY = iY;
 n=100;
@@ -25,15 +24,15 @@ for i=n:len
     end
 end
 
-figure(2)
-subplot(4,1,1);
-plot(sY(:,1));
-subplot(4,1,2);
-plot(sY(:,2));
-subplot(4,1,3);
-plot(sY(:,3));
-subplot(4,1,4);
-plot(sY(:,4));
+%figure(2)
+%subplot(4,1,1);
+%plot(sY(:,1));
+%subplot(4,1,2);
+%plot(sY(:,2));
+%subplot(4,1,3);
+%plot(sY(:,3));
+%subplot(4,1,4);
+%plot(sY(:,4));
 
 
 phase = 2.5;
@@ -59,52 +58,52 @@ for i=1:len
     phaseA(i) = phase;
 end
 %%
-figgy = figure(3);
-plot(phaseA, 'blue');
-grid on;
-tstart= 625000;
-tstop = 650000;
-margint=10000;
-marginY = 0.1;
-xlabel("time [Sample]")
-ylabel("Indexer");
-B = annotation('rectangle', 'LineStyle','--');
-B.Parent = figgy.CurrentAxes;
-B.Position = [tstart-margint, phaseA(tstart)-marginY, tstop-tstart+2*margint, phaseA(tstop) - phaseA(tstart)+2*marginY];
-a2 = axes();
-a2.Position = [0.2 0.65 0.2 0.2]; % xlocation, ylocation, xsize, ysize
-plot(a2,tstart:tstop,phaseA(tstart:tstop), 'blue');
-axis([tstart,tstop,phaseA(tstart)-marginY,phaseA(tstop)+marginY])
-grid on;
+%figgy = figure(3);
+%plot(phaseA, 'blue');
+%grid on;
+%tstart= 625000;
+%tstop = 650000;
+%margint=10000;
+%marginY = 0.1;
+%xlabel("time [Sample]")
+%ylabel("Indexer");
+%B = annotation('rectangle', 'LineStyle','--');
+%B.Parent = figgy.CurrentAxes;
+%B.Position = [tstart-margint, phaseA(tstart)-marginY, tstop-tstart+2*margint, phaseA(tstop) - phaseA(tstart)+2*marginY];
+%a2 = axes();
+%a2.Position = [0.2 0.65 0.2 0.2]; % xlocation, ylocation, xsize, ysize
+%plot(a2,tstart:tstop,phaseA(tstart:tstop), 'blue');
+%axis([tstart,tstop,phaseA(tstart)-marginY,phaseA(tstop)+marginY])
+%grid on;
 
 
 
 
 
-figure(4)
-plot(out);
+%figure(4)
+%plot(out);
 
 %%
-figure(5);
-SampleLength=5000;
-Overlap = 0.2*SampleLength;
-g = bartlett(SampleLength);
-F = logspace(0, log10(5000), 256);
-[s,f,t] = spectrogram(Y,g,Overlap,F,Fs);
-waterplot(s,f,t, 'cyan');
-hold on
-[s,f,t] = spectrogram(out,g,Overlap,F,Fs);
-waterplot(s,f,t+((t(2)-t(1))/2), 'r');
-axis([0,5000,0,26,0,2000]);
+%figure(5);
+%SampleLength=5000;
+%Overlap = 0.2*SampleLength;
+%g = bartlett(SampleLength);
+%F = logspace(0, log10(5000), 256);
+%[s,f,t] = spectrogram(Y,g,Overlap,F,Fs);
+%waterplot(s,f,t, 'cyan');
+%hold on
+%[s,f,t] = spectrogram(out,g,Overlap,F,Fs);
+%waterplot(s,f,t+((t(2)-t(1))/2), 'r');
+%axis([0,5000,0,26,0,2000]);
 
 
-function waterplot(s,f,t, color)
-% Waterfall plot of spectrogram
-    P = waterfall(f,t,abs(s)'.^2);
-    P.EdgeColor = color;
-    set(gca,XDir="reverse",View=[30 50])
-    xlabel("Frequency (Hz)")
-    ylabel("Time (s)")
-end
+%function waterplot(s,f,t, color)
+%%Waterfall plot of spectrogram
+%    P = waterfall(f,t,abs(s)'.^2);
+%    P.EdgeColor = color;
+%    set(gca,XDir="reverse",View=[30 50])
+%    xlabel("Frequency (Hz)")
+%    ylabel("Time (s)")
+%end
 %sound(out, Fs);
 
