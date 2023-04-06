cd6c5a1cf18c82654e7779286908fa7517643297
Daniel3E <124577043+Daniel3E@users.noreply.github.com>
2023-04-06 18:05:08 +0200

    Add files via upload

--- Workspace/Daniel/Project/matlab/source/EthernetImporter.m
+++ Workspace/Daniel/Project/matlab/source/EthernetImporter.m
@@ -1,21 +1,62 @@
 clc, clear all, close all
 
 
-InputFile = "ether.txt";
+InputFile = "Ether_Channel_2.txt";
+%InputFile = "ether.txt";
 fileId = fopen(InputFile, 'r');
 
 txt = fread(fileId, 'char*1');
 fclose(fileId);
+data = DataFinder(txt);
 
-for i = 1:2:length(txt)
-    A = tonum(txt(i))
-    B = tonum(txt(i+1))
-    
-    C = bitter(A,B);
-    D = chuffle(C);
-    E = bitToVal(D)
+mY=[]
+for i = 1:4:length(data)
+    A = tonum(data(i));
+    B = tonum(data(i+1));
+    C = tonum(data(i+2));
+    D = tonum(data(i+3));
+    mY = [mY,A*2^12 + B*2^8 + C*2^4 + D];
+    %C = bitter(A,B);
+    %E = bitToVal(C);
+    %D = chuffle(C);
 end
+q= 16;
+mY = mod(mY, 2^(q-1)) -(2^(q-1))*floor(mY./(2^(q-1)));
+subplot(4,1,1)
+plot(mY(1:4:end));
+subplot(4,1,2)
+plot(mY(2:4:end));
+subplot(4,1,3)
+plot(mY(3:4:end));
+subplot(4,1,4)
+plot(mY(4:4:end));
 
+%plot(mY(1:4:length(mY)));
+
+
+function Strong = DataFinder(A)
+r=[];
+
+    for a=20:length(A)
+        if A(a) == 13 && A(a-1) == 124
+            t=1+(3*10); %remove crc zeros
+            data=A(a-t-(3*8)+1:a-t); % every byte is 3 characters, 2 with data and a | seperator
+            DataArr = [];
+            for b=1:length(data)
+                if mod(b,3) ~= 0
+                    DataArr = [ DataArr, data(b)];
+                end
+            end
+            
+            r=[r,DataArr];
+            %break;
+        end
+        if a > 10000000
+            break;
+        end
+    end
+   Strong = r;
+end
 
 
 function A = bitToVal(B)
@@ -63,6 +104,8 @@ function A = tonum(charA)
         A = charA - 48;
     elseif charA>64 && charA<71
         A = charA -65+10;
+    elseif charA>96 && charA <103
+        A = charA -97 +10;
     end
 
 end
