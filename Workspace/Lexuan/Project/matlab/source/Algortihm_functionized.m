aa2b0c078b1224c77b8a8dc3d5d0061a9b750adc
Daniel3E <124577043+Daniel3E@users.noreply.github.com>
2023-04-13 17:23:28 +0200

    Add files via upload

--- Workspace/Lexuan/Project/matlab/source/Algortihm_functionized.m
+++ Workspace/Lexuan/Project/matlab/source/Algortihm_functionized.m
@@ -1,26 +1,108 @@
 clc, clear all, close all
 
-signal=[zeros(1,200), sin(linspace(0,40*pi, 10000))];
-figure(1)
-plot(signal);
-figure(2);
+%signal=[zeros(1,200), sin(linspace(0,40*pi, 10000))];
+%load('movingactor4-1_reading.mat'); % load mY
+load('Ether_Channel_3(1m_80dB_8s).mat')
 
-d = ceil(48000/343);
-signalAcc = zeros(1000,1);
-crossAcc = zeros(2*d,1);
-for i=1:1000
-    signalAcc = [signal(i); signalAcc(1:end-1)]; 
-
-
-    crossAcc = mycrossCorre(crossAcc, signalAcc,signalAcc,floor(length(crossAcc)/2));
-
-    hold on;
-    plot3(crossAcc, (1:length(crossAcc))-ceil(length(crossAcc)/2),i*ones(length(crossAcc),1), 'black');
-    [A,I] = max(crossAcc);
-    plot3(A,I-ceil(length(crossAcc)/2),i, 'o');
+pY = zeros(length(mY(:,1)),4);
+powAcc = zeros(1000,4);
+for i = 1:length(mY(:,1))
+    powAcc(:,1) = [abs(mY(i,1)); powAcc(1:end-1, 1)];
+    powAcc(:,2) = [abs(mY(i,2)); powAcc(1:end-1, 2)];
+    powAcc(:,3) = [abs(mY(i,3)); powAcc(1:end-1, 3)];
+    powAcc(:,4) = [abs(mY(i,4)); powAcc(1:end-1, 4)]; 
+    pY(i,1) = mean(powAcc(:,1));
+    pY(i,2) = mean(powAcc(:,2));
+    pY(i,3) = mean(powAcc(:,3));
+    pY(i,4) = mean(powAcc(:,4));
 end
 
+pY = pY./max(pY);
+%mY = mY./max(abs(mY));
 
+%highpass
+fY = zeros(length(mY(:,1)),4);
+fc = 200;
+[n,d] = butter(2, fc/(48000/2), 'high');
+sys = tf(n,d, 1/48000);
+fY(:,1) = lsim(sys, mY(:,1), []);
+fY(:,2) = lsim(sys, mY(:,2), []);
+fY(:,3) = lsim(sys, mY(:,3), []);
+fY(:,4) = lsim(sys, mY(:,4), []);
+
+%lowpass
+fc = 8000;
+[n,d] = butter(2, fc/(48000/2), 'low');
+sys = tf(n,d, 1/48000);
+fY(:,1) = lsim(sys, fY(:,1), []);
+fY(:,2) = lsim(sys, fY(:,2), []);
+fY(:,3) = lsim(sys, fY(:,3), []);
+fY(:,4) = lsim(sys, fY(:,4), []);
+
+mY=fY;
+
+figure(1)
+plot(mY);
+
+
+figure(2);
+len = length(mY(:,1));%48000;
+%len = 48000;
+d = ceil(48000/343);
+signalAcc = zeros(10000,4);
+crossAcc = zeros(4*d,3);
+lags = zeros(len,3);
+for i=1:len
+    signalAcc(:,1) = [mY(i,1); signalAcc(1:end-1,1)]; 
+    signalAcc(:,2) = [mY(i,2); signalAcc(1:end-1,2)]; 
+    signalAcc(:,3) = [mY(i,3); signalAcc(1:end-1,3)]; 
+    signalAcc(:,4) = [mY(i,4); signalAcc(1:end-1,4)]; 
+
+
+    crossAcc(:,1) = mycrossCorre(crossAcc(:,1), signalAcc(:,2),signalAcc(:,4),floor(length(crossAcc(:,1))/2)); % crosscorr mic 2 and 4
+    crossAcc(:,2) = mycrossCorre(crossAcc(:,2), signalAcc(:,2),signalAcc(:,3),floor(length(crossAcc(:,2))/2)); % mic 2 and 3
+    crossAcc(:,3) = mycrossCorre(crossAcc(:,3), signalAcc(:,3),signalAcc(:,4),floor(length(crossAcc(:,3))/2)); % mic 3 and 4
+    [A, lags(i,1)] = max(crossAcc(:,1));
+    [A, lags(i,2)] = max(crossAcc(:,2));
+    [A, lags(i,3)] = max(crossAcc(:,3));
+    if mod(i,1000) == 1
+        hold on;
+        n=1;
+        %plot3(crossAcc(:,1), (1:length(crossAcc(:,1)))-ceil(length(crossAcc(:,1))/2),i*ones(length(crossAcc(:,1)),1), 'black');
+        plot3(crossAcc(:,n), (1:length(crossAcc(:,n)))-ceil(length(crossAcc(:,n))/2),(i/48000)*ones(length(crossAcc(:,n)),1), 'color', [pY(i,3), pY(i,3), 0]);
+        [A,I] = max(crossAcc(:,n));
+        plot3(A,I-ceil(length(crossAcc(:,n))/2),i/48000, 'o');
+    end
+end
+
+figure(3)
+hold on
+plot(lags(:,1)-length(crossAcc(:,1))/2)
+plot(lags(:,2)-length(crossAcc(:,1))/2)
+plot(lags(:,3)-length(crossAcc(:,1))/2)
+figure(4)
+plot(mY(:,4))
+
+% figure(5)
+% for i=1:len
+%     delay2 = 343*(delay2+delay1)/48000;
+%     delay1 = 343*lags(i,3)/48000;
+% 
+%     A = 3 - 2;
+%     B = 4 - 3;
+% 
+%     x = -(B^2/delay2 - delay2- A^2/delay1 + delay1)/(2*(A/delay1 - B/delay2));
+%     
+%     D = ((-2*A-2*B)*x+ A^2 + B^2 - delay1^2 - delay2^2)/(2*(delay1 + delay2));
+% 
+%     y = sqrt(D^2 - x^2);
+% 
+%     x = Index1 -2.5 + x;
+%     if mod(i,1000) == 1
+%         plot(x,y, 'o');
+%     end
+% 
+% end
 
 
 
---
+++ Workspace/Lexuan/Project/matlab/source/EthernetImporter.m
@@ -0,0 +1,115 @@
+clc, clear all, close all
+
+
+InputFile = "Ether_Channel_3(1m_80dB_8s).txt";
+OutputFile = "Ether_Channel_3(1m_80dB_8s)";
+%OutputFile = "test"
+%InputFile = "ether.txt";
+fileId = fopen(InputFile, 'r');
+
+txt = fread(fileId, 'char*1');
+fclose(fileId);
+data = DataFinder(txt);
+
+mY=zeros(length(data)/4,1);
+for i = 1:4:length(data)
+    A = tonum(data(i));
+    B = tonum(data(i+1));
+    C = tonum(data(i+2));
+    D = tonum(data(i+3));
+    mY(ceil(i/4)) = A*2^12 + B*2^8 + C*2^4 + D;
+    %C = bitter(A,B);
+    %E = bitToVal(C);
+    %D = chuffle(C);
+end
+q= 16;
+mY = mod(mY, 2^(q-1)) -(2^(q-1))*floor(mY./(2^(q-1)));
+mY = [mY(1:4:end), mY(2:4:end), mY(3:4:end), mY(4:4:end)];
+subplot(4,1,1)
+plot(mY(:,1));
+subplot(4,1,2)
+plot(mY(:,2));
+subplot(4,1,3)
+plot(mY(:,3));
+subplot(4,1,4)
+plot(mY(:,4));
+save(OutputFile, "mY")
+%plot(mY(1:4:length(mY)));
+
+
+function Strong = DataFinder(A)
+r=[];
+
+    for a=20:length(A)
+        if A(a) == 13 & A(a-19:a-1) ==  double(['|'; '0'; '0'; '|'; '0'; '0'; '|'; '0'; '0'; '|'; '0'; '0'; '|'; '0'; '0'; '|'; '0'; '0'; '|'])
+            %A(a-10:a-1)
+            t=1+(3*10); %remove crc zeros
+            data=A(a-t-(3*8)+1:a-t); % every byte is 3 characters, 2 with data and a | seperator
+            DataArr = [];
+            for b=1:length(data)
+                if mod(b,3) ~= 0
+                    DataArr = [ DataArr, data(b)];
+                end
+            end
+            
+            r=[r,DataArr];
+            %break;
+        end
+%         if a > 10000000
+%             break;
+%         end
+    end
+   Strong = r;
+end
+
+
+function A = bitToVal(B)
+    b = 0;
+    for a=1:8
+        if B(a) == 1
+            b = b+2^(a-1);
+        end
+    end
+    A = b;
+end
+
+function A = chuffle(b)
+    a= zeros(8,1);
+    a(4) = b(5);
+    a(3) = b(6);
+    a(2) = b(7);
+    a(1) = b(8);
+    a(8) = b(1);
+    a(7) = b(2);
+    a(6) = b(3);
+    a(5) = b(4);
+    A = a;
+end
+
+
+function A = bitter(num1, num2)
+    b = zeros(8,1);
+    for a = 4:-1:1
+        if num1/(2^(a-1)) >= 1
+            b(a) = 1;
+            num1 = num1 -(2^(a-1));
+        end
+        if num2/(2^(a-1)) >= 1
+            b(a+4) = 1;
+            num2 = num2 -(2^(a-1));
+        end
+    end
+    A = b;
+end
+
+
+function A = tonum(charA)
+    if charA>47 && charA<58
+        A = charA - 48;
+    elseif charA>64 && charA<71
+        A = charA -65+10;
+    elseif charA>96 && charA <103
+        A = charA -97 +10;
+    end
+
+end
