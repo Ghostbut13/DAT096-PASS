cee0e6ce9867d32d5a90e8e748553aee330dc84f
Phyat22 <shihabi@chalmers.se>
2023-05-08 15:34:41 +0200

    -ammar finish switch.vhdl

--- Workspace/Ammar/Project/vhdl/source/Switch/switch.vhdl
+++ Workspace/Ammar/Project/vhdl/source/Switch/switch.vhdl
@@ -4,7 +4,7 @@
 --    cc2: mics 2 and 3 
 -- (-)cc2: mics 3 and 2
 --    cc3: mics 3 and 4
--- we output the two delta values being the max value from each correlation and the common index_out from the cross-corelation.
+-- we output the two delta values being the index of each MAX from each correlation and the common index_out from the cross-corelation.
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -14,36 +14,36 @@ use IEEE.NUMERIC_STD.ALL;
 
 ENTITY switch IS
   PORT  (
-    clk			: IN STD_LOGIC ;
-	rst_n 		: IN STD_LOGIC;
 	index_PE  	: IN STD_LOGIC_VECTOR (2 downto 0);
-	cc1_max 	: IN STD_LOGIC_VECTOR (15 downto 0);
-	cc2_MAX 	: IN STD_LOGIC_VECTOR (15 downto 0);
-	cc3_MAX 	: IN STD_LOGIC_VECTOR (15 downto 0);
+	cc1_max 	: IN STD_LOGIC_VECTOR (29 DOWNTO 0); -- 13 to 0 is index of the max value out of n inputs.
+	cc2_MAX 	: IN STD_LOGIC_VECTOR (29 DOWNTO 0);
+	cc3_MAX 	: IN STD_LOGIC_VECTOR (29 DOWNTO 0);
 	index_out	: OUT STD_LOGIC_VECTOR (2 downto 0);
-	delta1		: OUT STD_LOGIC_VECTOR (15 downto 0);
-    delta2 		: out std_logic_vector (15 downto 0)  
+	delta1		: OUT STD_LOGIC_VECTOR (13 downto 0);
+    delta2 		: out std_logic_vector (13 downto 0)  
     );
 END ENTITY switch;
 
 ARCHITECTURE ARCH_switch OF switch IS
 
-  SIGNAL delta1_out_signal: STD_LOGIC_VECTOR (15 DOWNTO 0);
-  SIGNAL delta2_out_signal: STD_LOGIC_VECTOR (15 DOWNTO 0);
+  signal cc1_index_signal : STD_LOGIC_VECTOR (13 downto 0);
+  signal cc2_index_signal : STD_LOGIC_VECTOR (13 downto 0);
+  signal cc3_index_signal : STD_LOGIC_VECTOR (13 downto 0);
+
   begin
- 
-  delta1_out_signal <= cc1_max when (conv_integer(index_PE) < 3) else
-  					   cc2_MAX;
+  
+	delta1 <= cc1_index_signal when (conv_integer(index_PE) < 3) else
+				   cc2_index_signal;
 
-  delta2_out_signal <= cc2_max when (conv_integer(index_PE) < 3) else
-					   cc3_MAX;
-						 
-	indexer_proc:
-   	 process(clk, rst_n)
-	 begin
-		
- 
- delta1 <= delta1_out_signal;
- delta2 <= delta2_out_signal;
+	delta2 <= cc2_index_signal when (conv_integer(index_PE) < 3) else
+				   cc3_index_signal;
+				   
+	index_out <= "010" when (conv_integer(index_PE) < 3) else
+				   "011";
 
+				   
+	cc1_index_signal <= cc1_max(13 downto 0);
+    cc2_index_signal <= cc2_max(13 downto 0);
+    cc3_index_signal <= cc3_max(13 downto 0);
+	
 END ARCH_switch;
