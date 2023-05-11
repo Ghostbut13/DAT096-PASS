f92c2beeb2604f3ff7ce4ca82828d30f65c1a4fc
Daniel3E <124577043+Daniel3E@users.noreply.github.com>
2023-05-11 11:30:27 +0200

    Add files via upload

--- Workspace/Daniel/Project/vhdl/source/crossAccExporter.m
+++ Workspace/Daniel/Project/vhdl/source/crossAccExporter.m
@@ -11,7 +11,9 @@ plot(crossAcc(:,1))
 plot(crossAcc(:,2))
 plot(crossAcc(:,3))
 
-crossAcc = mod(crossAcc, 2^(res-1)) -(2^(res-1))*floor(crossAcc./(2^(res-1)));
+crossAcc(:,1) = mod(crossAcc(:,1), 2^(res-1)) -(2^(res-1))*floor(crossAcc(:,1)./(2^(res-1)));
+crossAcc(:,2) = mod(crossAcc(:,2), 2^(res-1)) -(2^(res-1))*floor(crossAcc(:,2)./(2^(res-1)));
+crossAcc(:,3) = mod(crossAcc(:,3), 2^(res-1)) -(2^(res-1))*floor(crossAcc(:,3)./(2^(res-1)));
 
 str1="";
 str2="";
@@ -21,16 +23,16 @@ for i=1:length(crossAcc(:,1))
     B = dec2bin(abs(crossAcc(i,2)), res);
     C = dec2bin(abs(crossAcc(i,3)), res);
     str1 = str1+A + "\n";
-    str2 = str1+B + "\n";
-    str3 = str1+C + "\n";
+    str2 = str2+B + "\n";
+    str3 = str3+C + "\n";
 end
 
 fileId = fopen("corr1.txt", 'w');
 fprintf(fileId, str1);
 fclose(fileId);
 fileId = fopen("corr2.txt", 'w');
-fprintf(fileId, str1);
+fprintf(fileId, str2);
 fclose(fileId);
 fileId = fopen("corr3.txt", 'w');
-fprintf(fileId, str1);
+fprintf(fileId, str3);
 fclose(fileId);
